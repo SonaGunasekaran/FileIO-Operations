e2df25f422ec3fd65751a5bd1e0da97116346910
[Sona] <[sona16061999@gmail.com]>
2021-07-25 23:39:38 +0530

    Initialised with Welcome message

---
+++ FileOperations/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("File IO operations!");
+        }
+    }
+}
